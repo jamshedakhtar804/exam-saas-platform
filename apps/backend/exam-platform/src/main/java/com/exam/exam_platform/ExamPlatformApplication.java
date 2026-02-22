ffc5fa23675f957ba2975f469d3cd6f400f11859
Jamshed Akhter <jamshedakhtar804@gmail.com>
2026-02-22 22:11:13 +0530

    Initial repo structure + spring boot setup

---
+++ apps/backend/exam-platform/src/main/java/com/exam/exam_platform/ExamPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.exam.exam_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamPlatformApplication.class, args);
+	}
+
+}
---
+++ apps/backend/exam-platform/src/test/java/com/exam/exam_platform/ExamPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exam.exam_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
